fe77cb5bb99520df63ca7de137a8a6b393cae3b3
Alexander Wong <admin@alexander-wong.com>
2016-02-20 13:18:39 -0700

    Added phone call full information

--- app/src/main/java/com/github/hhyeg2016/mon/monitor/MonitorServiceThread.java
+++ app/src/main/java/com/github/hhyeg2016/mon/monitor/MonitorServiceThread.java
@@ -1,14 +1,8 @@
 package com.github.hhyeg2016.mon.monitor;
 
 import android.content.Context;
-import android.util.Log;
-
-import com.github.hhyeg2016.mon.phone.PhoneLogger;
 
 public class MonitorServiceThread extends Thread {
-    public static String SERVICE_THREAD_HASH = "SERVICE_THREAD_HASH";
-    public static String SERVICE_THREAD_PING_COUNT = "SERVICE_THREAD_PING_COUNT";
-
     private static Context context;
 
     private static int pingCount = 0;
@@ -22,11 +16,6 @@ public class MonitorServiceThread extends Thread {
 
     public void run() {
         do {
-            Log.i(SERVICE_THREAD_HASH, "id: " + this.hashCode());
-            Log.i(SERVICE_THREAD_PING_COUNT, "pc: " + pingCount);
-
-            PhoneLogger.getPhoneLogs(MonitorServiceThread.context);
-
             pingCount++;
             try {
                 Thread.sleep(1000);
--- app/src/main/java/com/github/hhyeg2016/mon/phone/PhoneLogger.java
+++ app/src/main/java/com/github/hhyeg2016/mon/phone/PhoneLogger.java
@@ -8,10 +8,14 @@ import android.provider.CallLog;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
+import java.util.Arrays;
 import java.util.Date;
 
 public class PhoneLogger {
+    public static String PHONE = "PHONE_PING";
+
     public static void getPhoneLogs(Context context) {
+        Log.i(PHONE, "RUNNING");
         try {
             Cursor c = context.getContentResolver().query(
                     CallLog.Calls.CONTENT_URI, null, null, null, null
@@ -20,25 +24,55 @@ public class PhoneLogger {
                     PackageManager.PERMISSION_GRANTED) {
                 // NO PERMISSIONS
                 // TODO. Handle permissions
+                Log.i(PHONE, "Read call log not set");
                 return;
             }
+            /*
+            // Column name types
+             [contactid, logtype, sim_id, sec_record, real_phone_number, presentation,
+              remind_me_later_set, e164_number, call_out_duration, vvm_id, countryiso,
+              dormant_set, photo_id, type, is_read, address, number, photoring_uri, cityid,
+              m_content, sdn_alpha_id, subscription_component_name, name, normalized_number,
+              sec_custom1, raw_contact_id, simnum, country_code, fname, formatted_number,
+              numbertype, sec_custom2, sns_tid, duration, callplus, account_id, geocoded_location,
+              transcription, lookup_uri, cdnip_number, sns_pkey, frequent, messageid,
+              subscription_id, _id, bname, sns_receiver_count, sp_type, pinyin_name, cnap_name,
+              features, voicemail_uri, new, sec_groupid, sec_custom3, date, data_usage,
+              numberlabel, reject_flag, service_type, m_subject, spam_report, matched_number,
+              account_name, lname]
+            */
+
             int number = c.getColumnIndex(CallLog.Calls.NUMBER);
             int type = c.getColumnIndex(CallLog.Calls.TYPE);
             int date = c.getColumnIndex(CallLog.Calls.DATE);
             int duration = c.getColumnIndex(CallLog.Calls.DURATION);
-
+            Log.i(PHONE, Arrays.toString(c.getColumnNames()));
             while (c.moveToNext()) {
                 String phNum = c.getString(number);
-                String callType = c.getString(type);
+                int callType = c.getInt(type);
                 String callDate = c.getString(date);
                 Date callDayTime = new Date(Long.valueOf(callDate));
                 String callDuration = c.getString(duration);
-                Log.i("PHONE", phNum + " " + callType + " " + callDate + " " + callDuration);
+
+                String sCallType = "Unknown";
+                switch (callType) {
+                    case CallLog.Calls.OUTGOING_TYPE:
+                        sCallType = "Outgoing";
+                        break;
+                    case CallLog.Calls.INCOMING_TYPE:
+                        sCallType = "Incoming";
+                        break;
+                    case CallLog.Calls.MISSED_TYPE:
+                        sCallType = "Missed";
+                        break;
+                }
+
+                Log.i(PHONE, phNum + ", " + sCallType + ", " + callDayTime.toString() + ", " + callDuration);
             }
 
             c.close();
         } catch (Exception e) {
-            Log.i("getPhoneLogs", e.toString());
+            Log.i(PHONE, e.toString());
         }
     }
 }
