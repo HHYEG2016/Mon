96c7289473a89940445a3c568caad8746ed9b154
Alexander Wong <admin@alexander-wong.com>
2016-02-20 15:46:58 -0700

    Refactoring the persistance

--- app/src/main/java/com/github/hhyeg2016/mon/data_manager/StoringManager.java
+++ app/src/main/java/com/github/hhyeg2016/mon/data_manager/StoringManager.java
@@ -5,24 +5,9 @@ import java.util.UUID;
 
 public abstract class StoringManager<A> {
 
-    public Boolean existsLocally(UUID id) {
-        A object = getById(id);
-        return object != null;
-    }
-
-    public void sync(A object, UUID id) {
-        if (existsLocally(id)) {
-            update(object);
-        } else {
-            insert(object);
-        }
-    }
-
     public abstract void insert(A object);
 
     public abstract ArrayList<A> retrieveAll();
-
-    public abstract void update(A newObject);
 
     public abstract A getById(UUID id);
 
