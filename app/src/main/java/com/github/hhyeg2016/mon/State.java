07381ada01e93364a46765cb43b18d50d00664c0
dhaberst <daniel.haberstock1@gmail.com>
2016-02-20 11:08:21 -0700

    added DataManager/Collection/Types

---
+++ app/src/main/java/com/github/hhyeg2016/mon/State.java
@@ -0,0 +1,24 @@
+package com.github.hhyeg2016.mon;
+
+/**
+ * Created by Daniel on 2016-02-20.
+ */
+public class State {
+    private static State ourInstance = new State();
+    private DataCollection dataCollection;
+
+    private State() {
+    }
+
+    public static State getInstance() {
+        return ourInstance;
+    }
+
+    public DataCollection getDataCollection() {
+        return dataCollection;
+    }
+
+    public void setDataCollection(DataCollection dataCollection) {
+        this.dataCollection = dataCollection;
+    }
+}
