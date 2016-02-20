8e117c096e823a99c1fe06d8a5c1180d5f5d0fe7
Alexander Wong <admin@alexander-wong.com>
2016-02-20 12:35:48 -0700

    Added phone log thing

--- app/src/main/java/com/github/hhyeg2016/mon/monitor/MonitorServiceThread.java
+++ app/src/main/java/com/github/hhyeg2016/mon/monitor/MonitorServiceThread.java
@@ -3,6 +3,8 @@ package com.github.hhyeg2016.mon.monitor;
 import android.content.Context;
 import android.util.Log;
 
+import com.github.hhyeg2016.mon.phone.PhoneLogger;
+
 public class MonitorServiceThread extends Thread {
     public static String SERVICE_THREAD_HASH = "SERVICE_THREAD_HASH";
     public static String SERVICE_THREAD_PING_COUNT = "SERVICE_THREAD_PING_COUNT";
@@ -22,6 +24,9 @@ public class MonitorServiceThread extends Thread {
         do {
             Log.i(SERVICE_THREAD_HASH, "id: " + this.hashCode());
             Log.i(SERVICE_THREAD_PING_COUNT, "pc: " + pingCount);
+
+            PhoneLogger.getPhoneLogs(MonitorServiceThread.context);
+
             pingCount++;
             try {
                 Thread.sleep(1000);
