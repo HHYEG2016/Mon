4af9e88905ae6a82e2288fd24c5091d3a61763ca
Conner D <connerdunn7399@gmail.com>
2016-02-20 13:08:06 -0700

    Basic UsageStatsManger is printing to logs

--- app/src/main/java/com/github/hhyeg2016/mon/monitor/MonitorServiceThread.java
+++ app/src/main/java/com/github/hhyeg2016/mon/monitor/MonitorServiceThread.java
@@ -1,7 +1,12 @@
 package com.github.hhyeg2016.mon.monitor;
 
 import android.content.Context;
+import android.app.usage.UsageEvents;
+import android.app.usage.UsageStatsManager;
 import android.util.Log;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MonitorServiceThread extends Thread {
     public static String SERVICE_THREAD_HASH = "SERVICE_THREAD_HASH";
@@ -10,6 +15,7 @@ public class MonitorServiceThread extends Thread {
     private static Context context;
 
     private static int pingCount = 0;
+    private static int REFRESH_RATE = 1000;
 
     public MonitorServiceThread() {}
 
@@ -22,9 +28,10 @@ public class MonitorServiceThread extends Thread {
         do {
             Log.i(SERVICE_THREAD_HASH, "id: " + this.hashCode());
             Log.i(SERVICE_THREAD_PING_COUNT, "pc: " + pingCount);
+            logUStats();
             pingCount++;
             try {
-                Thread.sleep(1000);
+                Thread.sleep(REFRESH_RATE);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -33,5 +40,33 @@ public class MonitorServiceThread extends Thread {
 
     public static void main(String args[]) {
         (new MonitorServiceThread()).start();
+    }
+
+
+    // Note user must enable permissions through "Settings > Security > Apps with usage access" first
+    public static void logUStats(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("M-d-yyyy HH:mm:ss");
+        String TAG = "logUStats";
+
+        //noinspection ResourceType
+        UsageStatsManager usm = (UsageStatsManager) context.getSystemService("usagestats");
+
+        Calendar calendar = Calendar.getInstance();
+        long endTime = calendar.getTimeInMillis();
+        calendar.add(Calendar.MILLISECOND, -REFRESH_RATE);
+        long startTime = calendar.getTimeInMillis();
+
+        Log.d(TAG, "Range start:" + dateFormat.format(startTime) );
+        Log.d(TAG, "Range end:" + dateFormat.format(endTime));
+
+        UsageEvents uEvents = usm.queryEvents(startTime,endTime);
+        while (uEvents.hasNextEvent()){
+            UsageEvents.Event e = new UsageEvents.Event();
+            uEvents.getNextEvent(e);
+
+            if (e != null){
+                Log.d(TAG, "Event: " + e.getPackageName() + "\t" +  e.getTimeStamp());
+            }
+        }
     }
 }
