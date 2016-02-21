3de9e75b9e36caec7d0a5f4128838b2be17f54ad
Alexander Wong <admin@alexander-wong.com>
2016-02-21 09:12:39 -0700

    Added outgoing SMS

--- app/src/main/java/com/github/hhyeg2016/mon/data_logger/TextLogger.java
+++ app/src/main/java/com/github/hhyeg2016/mon/data_logger/TextLogger.java
@@ -15,6 +15,43 @@ import java.util.ArrayList;
 public class TextLogger {
     public static String TEXT = "TEXT_PING";
 
+    private static ArrayList<TextData> getTextLogsPerURI(Uri uriSMSURI, Context context, String type) {
+        ArrayList<TextData> tdList = new ArrayList<>();
+        Cursor c = context.getContentResolver().query(uriSMSURI, null, null, null, null);
+        if (c != null) {
+            while (c.moveToNext()) {
+                int date_idx = c.getColumnIndex("date");
+                int read_idx = c.getColumnIndex("read");
+                int addr_idx = c.getColumnIndex("address");
+
+                long date_val = -1;
+                if (date_idx != -1) {
+                    date_val = c.getLong(date_idx);
+                }
+
+                String state_val = "Unknown";
+                if (read_idx != -1) {
+                    if (c.getInt(read_idx) == 1) {
+                        state_val = "Read";
+                    } else if (c.getInt(read_idx) == 0) {
+                        state_val = "Unread";
+                    }
+                }
+
+                String addr_val = "Unknown";
+                if (addr_idx != -1) {
+                    addr_val = c.getString(addr_idx);
+                }
+                TextData td = new TextData(date_val, type, state_val, addr_val);
+                tdList.add(td);
+            }
+            c.close();
+        } else {
+            Log.i(TEXT, "no col names");
+        }
+        return tdList;
+    }
+
     public static ArrayList<TextData> getTextLogs(Context context) {
         ArrayList<TextData> tdList = new ArrayList<>();
         Log.i(TEXT, "RUNNING");
@@ -27,58 +64,9 @@ public class TextLogger {
                 return tdList;
             }
             Uri uriSMSURI = Uri.parse("content://sms/inbox");
-            Cursor c = context.getContentResolver().query(uriSMSURI, null, null, null, null);
-            if (c != null) {
-                /*
-                String[] colNames;
-                colNames = c.getColumnNames();
-                // Log.i(TEXT, Arrays.toString(colNames));
-                [_id, thread_id, address, person, date, date_sent, protocol, read, status, type,
-                reply_path_present, subject, body, service_center, locked, error_code, sub_id,
-                creator, seen, deletable, sim_slot, sim_imsi, hidden, group_id, group_type,
-                delivery_date, app_id, msg_id, callback_number, reserved, pri, teleservice_id,
-                link_url, svc_cmd, svc_cmd_content, roam_pending, spam_report, secret_mode,
-                safe_message, favorite]
-                 */
-                /*
-                while (c.moveToNext()) {
-                    Log.i(TEXT, "SINGLE MSG START");
-                    for (String colName : colNames) {
-                        Log.i(TEXT, "\t" + colName + ": " + c.getString(c.getColumnIndex(colName)));
-                    }
-                }
-                */
-                while (c.moveToNext()) {
-                    int date_idx = c.getColumnIndex("date");
-                    int read_idx = c.getColumnIndex("read");
-                    int addr_idx = c.getColumnIndex("address");
-
-                    long date_val = -1;
-                    if (date_idx != -1) {
-                        date_val = c.getLong(date_idx);
-                    }
-
-                    String state_val = "Unknown";
-                    if (read_idx != -1) {
-                        if (c.getInt(read_idx) == 1) {
-                            state_val = "Read";
-                        } else if (c.getInt(read_idx) == 0) {
-                            state_val = "Unread";
-                        }
-                    }
-
-                    String addr_val = "Unknown";
-                    if (addr_idx != -1) {
-                        addr_val = c.getString(addr_idx);
-                    }
-                    TextData td = new TextData(date_val, "Incoming", state_val, addr_val);
-                    tdList.add(td);
-                }
-                c.close();
-            } else {
-                Log.i(TEXT, "no col names");
-            }
-
+            tdList.addAll(getTextLogsPerURI(uriSMSURI, context, "Incoming"));
+            uriSMSURI = Uri.parse("content://sms/sent");
+            tdList.addAll(getTextLogsPerURI(uriSMSURI, context, "Outgoing"));
         } catch (Exception e) {
             Log.i(TEXT, e.toString());
         }
