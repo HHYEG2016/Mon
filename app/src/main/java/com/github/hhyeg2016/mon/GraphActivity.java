90a6bffc6217e4a7b2561ed0e02b20c3a15e7067
Conner D <connerdunn7399@gmail.com>
2016-02-20 21:41:35 -0700

    A better starting point for our graphing

--- app/src/main/java/com/github/hhyeg2016/mon/GraphActivity.java
+++ app/src/main/java/com/github/hhyeg2016/mon/GraphActivity.java
@@ -30,66 +30,28 @@ import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.listener.ChartTouchListener;
 import com.github.mikephil.charting.listener.OnChartGestureListener;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-public class GraphActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, OnChartGestureListener, OnChartValueSelectedListener {
+public class GraphActivity extends AppCompatActivity {
 
     private LineChart mChart;
-    private SeekBar mSeekBarX, mSeekBarY;
-    private TextView tvX, tvY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_linechart);
-
-        tvX = (TextView) findViewById(R.id.tvXMax);
-        tvY = (TextView) findViewById(R.id.tvYMax);
-
-        mSeekBarX = (SeekBar) findViewById(R.id.seekBar1);
-        mSeekBarY = (SeekBar) findViewById(R.id.seekBar2);
-
-        mSeekBarX.setProgress(45);
-        mSeekBarY.setProgress(100);
-
-        mSeekBarY.setOnSeekBarChangeListener(this);
-        mSeekBarX.setOnSeekBarChangeListener(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         mChart = (LineChart) findViewById(R.id.chart1);
-        mChart.setOnChartGestureListener(this);
-        mChart.setOnChartValueSelectedListener(this);
-        mChart.setDrawGridBackground(false);
 
         // no description text
         mChart.setDescription("");
         mChart.setNoDataTextDescription("You need to provide data for the chart.");
-
-        // enable touch gestures
-        mChart.setTouchEnabled(true);
-
-        // enable scaling and dragging
-        mChart.setDragEnabled(true);
-        mChart.setScaleEnabled(true);
-        // mChart.setScaleXEnabled(true);
-        // mChart.setScaleYEnabled(true);
-
-        // if disabled, scaling can be done on x- and y-axis separately
-        mChart.setPinchZoom(true);
-
-        // set an alternative background color
-        // mChart.setBackgroundColor(Color.GRAY);
-
-        // create a custom MarkerView (extend MarkerView) and specify the layout
-        // to use for it
-        //MyMarkerView mv = new MyMarkerView(this, R.layout.custom_marker_view);  // debugging
-
-        // set the marker to the chart
-        //mChart.setMarkerView(mv);  // debugging
 
         // x-axis limit line
         LimitLine llXAxis = new LimitLine(10f, "Index 10");
@@ -99,31 +61,27 @@ public class GraphActivity extends AppCompatActivity implements SeekBar.OnSeekBa
         llXAxis.setTextSize(10f);
 
         XAxis xAxis = mChart.getXAxis();
-        //xAxis.setValueFormatter(new MyCustomXAxisValueFormatter());
-        //xAxis.addLimitLine(llXAxis); // add x-axis limit line
 
-        //Typeface tf = Typeface.createFromAsset(getAssets(), "OpenSans-Regular.ttf");
 
-        LimitLine ll1 = new LimitLine(130f, "Upper Limit");
+        LimitLine ll1 = new LimitLine(180f, "Above action is suggested");
         ll1.setLineWidth(4f);
         ll1.enableDashedLine(10f, 10f, 0f);
         ll1.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
         ll1.setTextSize(10f);
-        //ll1.setTypeface(tf);
 
-        LimitLine ll2 = new LimitLine(-30f, "Lower Limit");
+        LimitLine ll2 = new LimitLine(150f, "Below is excellent");
         ll2.setLineWidth(4f);
         ll2.enableDashedLine(10f, 10f, 0f);
         ll2.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
         ll2.setTextSize(10f);
-        //ll2.setTypeface(tf);
 
         YAxis leftAxis = mChart.getAxisLeft();
         leftAxis.removeAllLimitLines(); // reset all limit lines to avoid overlapping lines
-        leftAxis.addLimitLine(ll1);
-        leftAxis.addLimitLine(ll2);
-        leftAxis.setAxisMaxValue(220f);
-        leftAxis.setAxisMinValue(-50f);
+//        leftAxis.addLimitLine(ll1);
+//        leftAxis.addLimitLine(ll2);
+        leftAxis.setAxisMaxValue(10f);
+        leftAxis.setAxisMinValue(0f);
+        leftAxis.setStartAtZero(false);
         //leftAxis.setYOffset(20f);
         leftAxis.enableGridDashedLine(10f, 10f, 0f);
         leftAxis.setDrawZeroLine(false);
@@ -137,53 +95,85 @@ public class GraphActivity extends AppCompatActivity implements SeekBar.OnSeekBa
         //mChart.getViewPortHandler().setMaximumScaleX(2f);
 
         // add data
-        setData(45, 100);
-
-//        mChart.setVisibleXRange(20);
-//        mChart.setVisibleYRange(20f, AxisDependency.LEFT);
-//        mChart.centerViewTo(20, 50, AxisDependency.LEFT);
-
-        mChart.animateX(2500, Easing.EasingOption.EaseInOutQuart);
-//        mChart.invalidate();
-
-        // get the legend (only possible after setting data)
-        Legend l = mChart.getLegend();
-
-        // modify the legend ...
-        // l.setPosition(LegendPosition.LEFT_OF_CHART);
-        l.setForm(Legend.LegendForm.LINE);
-
-        // // dont forget to refresh the drawing
-        // mChart.invalidate();
-    }
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-    }
-
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
-        tvX.setText("" + (mSeekBarX.getProgress() + 1));
-        tvY.setText("" + (mSeekBarY.getProgress()));
-
-        setData(mSeekBarX.getProgress() + 1, mSeekBarY.getProgress());
-
-        // redraw
-        mChart.invalidate();
-    }
-
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
-        // TODO Auto-generated method stub
+        setData(10, 100);
 
     }
-
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        getMenuInflater().inflate(R.menu.graph_menu, menu);
+        return true;
+    }
     @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
-        // TODO Auto-generated method stub
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+        switch (item.getItemId()) {
+            case R.id.actionToggleValues: {
+                List<ILineDataSet> sets = mChart.getData()
+                        .getDataSets();
+
+                for (ILineDataSet iSet : sets) {
+
+                    LineDataSet set = (LineDataSet) iSet;
+                    set.setDrawValues(!set.isDrawValuesEnabled());
+                }
+
+                mChart.invalidate();
+                break;
+            }
+            case R.id.actionToggleHighlight: {
+                if(mChart.getData() != null) {
+                    mChart.getData().setHighlightEnabled(!mChart.getData().isHighlightEnabled());
+                    mChart.invalidate();
+                }
+                break;
+            }
+            case R.id.actionToggleFilled: {
+
+                List<ILineDataSet> sets = mChart.getData()
+                        .getDataSets();
+
+                for (ILineDataSet iSet : sets) {
+
+                    LineDataSet set = (LineDataSet) iSet;
+                    if (set.isDrawFilledEnabled())
+                        set.setDrawFilled(false);
+                    else
+                        set.setDrawFilled(true);
+                }
+                mChart.invalidate();
+                break;
+            }
+            case R.id.actionToggleCircles: {
+                List<ILineDataSet> sets = mChart.getData()
+                        .getDataSets();
+
+                for (ILineDataSet iSet : sets) {
+
+                    LineDataSet set = (LineDataSet) iSet;
+                    if (set.isDrawCirclesEnabled())
+                        set.setDrawCircles(false);
+                    else
+                        set.setDrawCircles(true);
+                }
+                mChart.invalidate();
+                break;
+            }
+            case R.id.actionToggleCubic: {
+                List<ILineDataSet> sets = mChart.getData()
+                        .getDataSets();
+
+                for (ILineDataSet iSet : sets) {
+
+                    LineDataSet set = (LineDataSet) iSet;
+                    if (set.isDrawCubicEnabled())
+                        set.setDrawCubic(false);
+                    else
+                        set.setDrawCubic(true);
+                }
+                mChart.invalidate();
+                break;
+            }
+        }
+        return true;
     }
 
     private void setData(int count, float range) {
@@ -193,98 +183,185 @@ public class GraphActivity extends AppCompatActivity implements SeekBar.OnSeekBa
             xVals.add((i) + "");
         }
 
-        ArrayList<Entry> yVals = new ArrayList<Entry>();
+        ArrayList<Entry> yVals1 = new ArrayList<Entry>();
 
         for (int i = 0; i < count; i++) {
 
-            float mult = (range + 1);
-            float val = (float) (Math.random() * mult) + 3;// + (float)
+            Random r = new Random();
+            int Low = 5;
+            int High = 9;
+            int Result = r.nextInt(High-Low) + Low;
+
+            float val = (float) (Result) ;// + (float)
             // ((mult *
             // 0.1) / 10);
-            yVals.add(new Entry(val, i));
+            yVals1.add(new Entry(val, i));
         }
 
         // create a dataset and give it a type
-        LineDataSet set1 = new LineDataSet(yVals, "DataSet 1");
+        LineDataSet phSet = new LineDataSet(yVals1, "pH");
+        phSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+        phSet.setColor(ColorTemplate.getHoloBlue());
+        phSet.setCircleColor(ColorTemplate.getHoloBlue());
+        phSet.setLineWidth(2f);
+        phSet.setCircleRadius(3f);
+        phSet.setFillAlpha(65);
+        phSet.setFillColor(ColorTemplate.getHoloBlue());
+        phSet.setHighLightColor(Color.rgb(244, 117, 117));
+        phSet.setDrawCircleHole(false);
+        //set1.setFillFormatter(new MyFillFormatter(0f));
+//        set1.setDrawHorizontalHighlightIndicator(false);
+//        set1.setVisible(false);
+//        set1.setCircleHoleColor(Color.WHITE);
+
+        ArrayList<Entry> yVals2 = new ArrayList<Entry>();
+
+        for (int i = 0; i < count; i++) {
+            Random r = new Random();
+            int Low = 0;
+            int High = 4;
+            int Result = r.nextInt(High-Low) + Low;
+
+            float val = (float) (Result) ;// + (float)
+            // ((mult *
+            // 0.1) / 10);
+            // ((mult *
+            // 0.1) / 10);
+            yVals2.add(new Entry(val, i));
+        }
+
+        // create a dataset and give it a type
+        LineDataSet ketonesSet = new LineDataSet(yVals2, "Ketones");
+        ketonesSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+        ketonesSet.setColor(Color.RED);
+        ketonesSet.setCircleColor(Color.RED);
+        ketonesSet.setLineWidth(2f);
+        ketonesSet.setCircleRadius(3f);
+        ketonesSet.setFillAlpha(65);
+        ketonesSet.setFillColor(Color.RED);
+        ketonesSet.setDrawCircleHole(false);
+        ketonesSet.setHighLightColor(Color.rgb(244, 117, 117));
+        //set2.setFillFormatter(new MyFillFormatter(900f));
+
+        //        ArrayList<String> xVals = new ArrayList<String>();
+//        for (int i = 0; i < count; i++) {
+//            xVals.add((i) + "");
+//        }
+//
+        ArrayList<Entry> yVals3 = new ArrayList<Entry>();
+
+        for (int i = 0; i < count; i++) {
+
+            Random r = new Random();
+            int Low = 0;
+            int High = 3;
+            int Result = r.nextInt(High-Low) + Low;
+
+            float val = (float) (Result) ;// + (float)
+            // ((mult *
+            // 0.1) / 10);
+            yVals3.add(new Entry(val, i));
+        }
+
+        // create a dataset and give it a type
+//        LineDataSet proteinSet = new LineDataSet(yVals3, "Protein");
+        // set1.setFillAlpha(110);
+        // set1.setFillColor(Color.RED);
+        LineDataSet proteinSet = new LineDataSet(yVals3, "Protein");
+        proteinSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+        proteinSet.setColor(Color.DKGRAY);
+        proteinSet.setCircleColor(Color.DKGRAY);
+        proteinSet.setLineWidth(2f);
+        proteinSet.setCircleRadius(3f);
+        proteinSet.setFillAlpha(65);
+        proteinSet.setFillColor(Color.DKGRAY);
+        proteinSet.setDrawCircleHole(false);
+        proteinSet.setHighLightColor(Color.rgb(244, 117, 117));
+//
+
+        ArrayList<Entry> yVals4 = new ArrayList<Entry>();
+
+        for (int i = 0; i < count; i++) {
+
+            Random r = new Random();
+            int Low = 0;
+            int High =5;
+            int Result = r.nextInt(High-Low) + Low;
+
+            float val = (float) (Result) ;// + (float)
+            // ((mult *
+            // 0.1) / 10);
+            yVals4.add(new Entry(val, i));
+        }
+
+        // create a dataset and give it a type
+        LineDataSet hemoglobinSet = new LineDataSet(yVals4, "Hemoglobin");
         // set1.setFillAlpha(110);
         // set1.setFillColor(Color.RED);
 
-        // set the line to be drawn like this "- - - - - -"
-        set1.enableDashedLine(10f, 5f, 0f);
-        set1.enableDashedHighlightLine(10f, 5f, 0f);
-        set1.setColor(Color.BLACK);
-        set1.setCircleColor(Color.BLACK);
-        set1.setLineWidth(1f);
-        set1.setCircleRadius(3f);
-        set1.setDrawCircleHole(false);
-        set1.setValueTextSize(9f);
-        //Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_red);
-        //set1.setFillDrawable(drawable);
-        set1.setDrawFilled(true);
+//        // set the line to be drawn like this "- - - - - -"
+        hemoglobinSet.enableDashedLine(10f, 5f, 0f);
+        hemoglobinSet.enableDashedHighlightLine(10f, 5f, 0f);
+        hemoglobinSet.setColor(Color.MAGENTA);
+        hemoglobinSet.setCircleColor(Color.MAGENTA);
+        hemoglobinSet.setLineWidth(1f);
+        hemoglobinSet.setCircleRadius(3f);
+        hemoglobinSet.setDrawCircleHole(false);
+        hemoglobinSet.setValueTextSize(9f);
 
+        ArrayList<Entry> yVals5 = new ArrayList<Entry>();
+
+        for (int i = 0; i < count; i++) {
+
+            Random r = new Random();
+            int Low = 0;
+            int High = 5;
+            int Result = r.nextInt(High - Low) + Low;
+
+            float val = (float) (Result) ;// + (float)
+            // ((mult *
+            // 0.1) / 10);
+            yVals5.add(new Entry(val, i));
+        }
+
+        // create a dataset and give it a type
+        LineDataSet glucoseSet = new LineDataSet(yVals5, "Glucose");
+        // set1.setFillAlpha(110);
+        // set1.setFillColor(Color.RED);
+
+//        // set the line to be drawn like this "- - - - - -"
+        glucoseSet.enableDashedLine(10f, 5f, 0f);
+        glucoseSet.enableDashedHighlightLine(10f, 5f, 0f);
+        glucoseSet.setColor(Color.CYAN);
+        glucoseSet.setCircleColor(Color.CYAN);
+        glucoseSet.setLineWidth(1f);
+        glucoseSet.setCircleRadius(3f);
+        glucoseSet.setDrawCircleHole(false);
+        glucoseSet.setValueTextSize(9f);
+//        ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
         ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
-        dataSets.add(set1); // add the datasets
+        dataSets.add(ketonesSet);
+        dataSets.add(glucoseSet);
+        dataSets.add(proteinSet);
+
+        dataSets.add(phSet); // add the datasets
+        dataSets.add(hemoglobinSet); // add the datasets
 
         // create a data object with the datasets
         LineData data = new LineData(xVals, dataSets);
+        data.setValueTextColor(Color.BLACK);
+        data.setValueTextSize(9f);
+//
+//        // create a data object with the datasets
+//        LineData data = new LineData(xVals, dataSets);
+//
+//        // set data
+//        mChart.setData(data);
+
 
         // set data
         mChart.setData(data);
     }
 
-    @Override
-    public void onChartGestureStart(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
-        Log.i("Gesture", "START");
-    }
-
-    @Override
-    public void onChartGestureEnd(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
-        Log.i("Gesture", "END, lastGesture: " + lastPerformedGesture);
-
-        // un-highlight values after the gesture is finished and no single-tap
-        if(lastPerformedGesture != ChartTouchListener.ChartGesture.SINGLE_TAP)
-            mChart.highlightValues(null); // or highlightTouch(null) for callback to onNothingSelected(...)
-    }
-
-    @Override
-    public void onChartLongPressed(MotionEvent me) {
-        Log.i("LongPress", "Chart longpressed.");
-    }
-
-    @Override
-    public void onChartDoubleTapped(MotionEvent me) {
-        Log.i("DoubleTap", "Chart double-tapped.");
-    }
-
-    @Override
-    public void onChartSingleTapped(MotionEvent me) {
-        Log.i("SingleTap", "Chart single-tapped.");
-    }
-
-    @Override
-    public void onChartFling(MotionEvent me1, MotionEvent me2, float velocityX, float velocityY) {
-        Log.i("Fling", "Chart flinged. VeloX: " + velocityX + ", VeloY: " + velocityY);
-    }
-
-    @Override
-    public void onChartScale(MotionEvent me, float scaleX, float scaleY) {
-        Log.i("Scale / Zoom", "ScaleX: " + scaleX + ", ScaleY: " + scaleY);
-    }
-
-    @Override
-    public void onChartTranslate(MotionEvent me, float dX, float dY) {
-        Log.i("Translate / Move", "dX: " + dX + ", dY: " + dY);
-    }
-
-    @Override
-    public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
-        Log.i("Entry selected", e.toString());
-        Log.i("LOWHIGH", "low: " + mChart.getLowestVisibleXIndex() + ", high: " + mChart.getHighestVisibleXIndex());
-        Log.i("MIN MAX", "xmin: " + mChart.getXChartMin() + ", xmax: " + mChart.getXChartMax() + ", ymin: " + mChart.getYChartMin() + ", ymax: " + mChart.getYChartMax());
-    }
-
-    @Override
-    public void onNothingSelected() {
-        Log.i("Nothing selected", "Nothing selected.");
-    }
 }
+
