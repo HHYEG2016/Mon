8f0245a3a6739e7fb3a747ab8018fcae3c2e1877
Alexander Wong <admin@alexander-wong.com>
2016-02-21 11:14:58 -0700

    janky bargraphs

--- app/src/main/java/com/github/hhyeg2016/mon/ChartAppDataActivity.java
+++ app/src/main/java/com/github/hhyeg2016/mon/ChartAppDataActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -25,13 +27,19 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Random;
+import java.util.Map;
 import java.util.Set;
 
 public class ChartAppDataActivity extends AppCompatActivity {
 
     private BarChart mChart;
+    private static int appNameSelectorIndex = 0;
+    private static int appDateSelectorIndex = 0;
+    private static String[] names;
+    private static String[] dates;
+    private static ArrayList<AppData> mData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,10 +74,8 @@ public class ChartAppDataActivity extends AppCompatActivity {
 
         //mChart.getViewPortHandler().setMaximumScaleY(2f);
         //mChart.getViewPortHandler().setMaximumScaleX(2f);
-
-        // add data
-        // TODO: ArrayList<AppData> set
-        setData(AppLogger.getAppDataLogs(getApplicationContext()));
+        mData = AppLogger.getAppDataLogs(getApplicationContext());
+        initializeData();
     }
 
     public boolean onCreateOptionsMenu(android.view.Menu menu) {
@@ -142,64 +148,11 @@ public class ChartAppDataActivity extends AppCompatActivity {
         return true;
     }
 
-    private void setData(ArrayList<AppData> adList) {
-        int numHours = 24;
-        ArrayList<String> xVals = new ArrayList<String>();
-        for (int i = 0; i < numHours; i++) {
-            xVals.add((i) + "");
-        }
-
-        ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
-
-        for (int i = 0; i < numHours; i++) {
-
-            Random r = new Random();
-            int Low = 5;
-            int High = 9;
-            int Result = r.nextInt(High - Low) + Low;
-
-            float val = (float) (Result);// + (float)
-            // ((mult *
-            // 0.1) / 10);
-            yVals1.add(new BarEntry(val, i));
-        }
-
-        // create a dataset and give it a type
-        BarDataSet phSet = new BarDataSet(yVals1, "testVal");
-        phSet.setAxisDependency(YAxis.AxisDependency.LEFT);
-        phSet.setColor(ColorTemplate.getHoloBlue());
-        phSet.setHighLightColor(Color.rgb(244, 117, 117));
-        phSet.setColors(ColorTemplate.JOYFUL_COLORS);
-        //set1.setFillFormatter(new MyFillFormatter(0f));
-//        set1.setDrawHorizontalHighlightIndicator(false);
-//        set1.setVisible(false);
-//        set1.setCircleHoleColor(Color.WHITE);
-
-
-        ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
-        dataSets.add(phSet); // add the datasets
-        // dataSets.add(phSet); // can have multiple data sets
-
-        // create a data object with the datasets
-        BarData data = new BarData(xVals, dataSets);
-        data.setValueTextColor(Color.BLACK);
-        data.setValueTextSize(9f);
-//
-//        // create a data object with the datasets
-//        LineData data = new LineData(xVals, dataSets);
-//
-//        // set data
-//        mChart.setData(data);
-
-
-        // set data
-        mChart.setData(data);
-
-
+    private void initializeData() {
         // Setting app name data
         Set<String> appNames = new HashSet<>();
         Set<String> appTimes = new HashSet<>();
-        for (AppData appData : adList) {
+        for (AppData appData : mData) {
             appNames.add(appData.getAppName());
             Calendar cl = Calendar.getInstance();
             cl.setTimeInMillis(appData.getLogTime());
@@ -207,19 +160,103 @@ public class ChartAppDataActivity extends AppCompatActivity {
             appTimes.add(date);
         }
         Spinner app_name_spinner = (Spinner) findViewById(R.id.app_name_spinner);
+        names = appNames.toArray(new String[appNames.size()]);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_spinner_item,
-                appNames.toArray(new String[appNames.size()])
+                names
         );
         // Apply the adapter to the spinner
         app_name_spinner.setAdapter(adapter);
         Spinner app_date_spinner = (Spinner) findViewById(R.id.ad_date_spinner);
+        dates = appTimes.toArray(new String[appTimes.size()]);
         ArrayAdapter<String> adapterDate = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_spinner_item,
-                appTimes.toArray(new String[appTimes.size()])
+                dates
         );
         app_date_spinner.setAdapter(adapterDate);
+
+        app_name_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                appNameSelectorIndex = position;
+                redraw_graph();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+            }
+
+        });
+
+        app_date_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                appDateSelectorIndex = position;
+                redraw_graph();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+            }
+
+        });
+    }
+
+    private void redraw_graph() {
+        String name = names[appNameSelectorIndex];
+        String date = dates[appDateSelectorIndex];
+
+
+        int numHours = 24;
+        ArrayList<String> xVals = new ArrayList<String>();
+        for (int i = 0; i < numHours; i++) {
+            xVals.add("hour: " + (i));
+        }
+        ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
+
+        HashMap<String, Integer> hmap = new HashMap<>();
+        for (AppData mDataVal : mData) {
+            if (mDataVal.getAppName().equals(name)) {
+                Calendar cl = Calendar.getInstance();
+                cl.setTimeInMillis(mDataVal.getLogTime());
+                String compVal = "" + cl.get(Calendar.DAY_OF_MONTH) + " " + cl.get(Calendar.MONTH) + " " + cl.get(Calendar.YEAR);
+                if(compVal.equals(date)) {
+
+                    String dval = "" + cl.get(Calendar.HOUR_OF_DAY);
+                    if (hmap.get(dval) == null) {
+                        hmap.put(dval, 0);
+                    } else {
+                        int curCount = hmap.get(dval);
+                        curCount++;
+                        hmap.put(dval, curCount);
+                    }
+                }
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : hmap.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            yVals1.add(new BarEntry(Integer.valueOf(key), value));
+        }
+
+
+        // create a dataset and give it a type
+        BarDataSet phSet = new BarDataSet(yVals1, "testVal");
+        phSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+        phSet.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+        dataSets.add(phSet);
+        // create a data object with the datasets
+        BarData data = new BarData(xVals, dataSets);
+        data.setValueTextColor(Color.BLACK);
+        data.setValueTextSize(9f);
+
+        // set data
+        mChart.setData(data);
+
     }
 }
